bd79267e39353fccca9df5f86ec4d66b4dacc542
mBentaifour <bentaifourmoh@gmail.com>
2024-10-19 13:56:31 -0700

    MySQL advanced

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- We are all unique!
+-- SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT, 
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
