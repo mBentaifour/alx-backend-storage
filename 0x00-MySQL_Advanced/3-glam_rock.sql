4cf4fd035907197f2a28078b4f58ecb4d5b8d01d
mbentaifour <bentaifourmoh@gmail.com>
2024-10-20 17:20:19 +0000

    MySQL Advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- Old school band
 -- script that lists all bands with Glam rock
 
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+SELECT band_name,
+       IFNULL(YEAR(split), 2022) - YEAR(formed) AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
