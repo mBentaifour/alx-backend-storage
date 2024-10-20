d7a8a8704ce112ab62b67a5daab176afa2f09d6e
mbentaifour <bentaifourmoh@gmail.com>
2024-10-20 17:39:27 +0000

    MySQL Advanced

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,16 @@
+-- Create a trigger to invalidate a user's email address when it is updated
+-- when the email changed
+-- Email validation to sent
+
+DELIMITER $$ 
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
+$$
+DELIMITER ;
